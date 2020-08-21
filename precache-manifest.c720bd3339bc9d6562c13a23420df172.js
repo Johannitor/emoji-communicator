cc631d439f57b645de8ef61c19f2d94fb8467acc
Johannitor <50795659+Johannitor@users.noreply.github.com>
2020-08-21 15:51:28 +0000

    Deploying to gh-pages from  @ 7207266c3c9122f4bc4c7ddffd304a8ce8990efd 🚀

---
+++ precache-manifest.c720bd3339bc9d6562c13a23420df172.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ada6aaab08f2a248dad5b1c7ed28c4e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "85870a1c7dcfdc798db2",
+    "url": "/static/css/main.1193d232.chunk.css"
+  },
+  {
+    "revision": "b3772ed9be777a50937f",
+    "url": "/static/js/2.66855037.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.66855037.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85870a1c7dcfdc798db2",
+    "url": "/static/js/main.e114f419.chunk.js"
+  },
+  {
+    "revision": "3f5c10aa89a09109aa6d",
+    "url": "/static/js/runtime-main.e50a6e43.js"
+  }
+]);
