7fc2975dc30e480839062e1e60213a4af79c82d1
Johann Lochbaum <johannlochbaum@outlook.de>
2020-08-22 22:51:54 +0200

    Fix emoji table not being able to scroll as the fullscreen view would constantly open

--- src/components/emoji-table/EmojiCard.tsx
+++ src/components/emoji-table/EmojiCard.tsx
@@ -10,17 +10,48 @@ type EmojiCardProps = {
 
 export function EmojiCard({ emoji, onClick }: EmojiCardProps) {
   const { toggleFavorite } = useEmojiFavories();
-  const [mouseStart, setMouseStart] = useState<number>();
 
-  const handleMouseDown = useCallback(() => {
+  const [mouseStart, setMouseStart] = useState<number>();
+  const [moved, setMoved] = useState<boolean>(false);
+  const [touchStartPos, setTouchStartPos] = useState<{
+    clientX: number;
+    clientY: number;
+  }>({
+    clientX: 0,
+    clientY: 0,
+  });
+
+  const handleCursorDown = useCallback(() => {
     setMouseStart(new Date().getTime());
+    setMoved(false);
   }, []);
 
-  const handleMouseUp = useCallback(() => {
-    if (mouseStart) {
-      setMouseStart(undefined);
+  const handleTouchStart = useCallback(
+    (e: React.TouchEvent<HTMLButtonElement>) => {
+      setTouchStartPos(e.touches[0]);
+      handleCursorDown();
+    },
+    [handleCursorDown]
+  );
 
+  const handleTouchMove = useCallback(
+    (e: React.TouchEvent<HTMLButtonElement>) => {
+      const { clientX, clientY } = e.touches[0];
+      if (
+        Math.abs(touchStartPos.clientX - clientX) > 10 ||
+        Math.abs(touchStartPos.clientY - clientY) > 10
+      ) {
+        setMoved(true);
+        setMouseStart(undefined);
+      }
+    },
+    [touchStartPos]
+  );
+
+  const handleCursorUp = useCallback(() => {
+    if (mouseStart && !moved) {
       const holdDownMs = new Date().getTime() - mouseStart;
+      setMouseStart(undefined);
 
       if (holdDownMs < 1000) {
         onClick(emoji);
@@ -28,18 +59,18 @@ export function EmojiCard({ emoji, onClick }: EmojiCardProps) {
         toggleFavorite(emoji);
       }
     }
-  }, [mouseStart, emoji, onClick, toggleFavorite]);
+  }, [mouseStart, moved, emoji, onClick, toggleFavorite]);
 
   useEffect(() => {
     let timeoutId: number;
     if (mouseStart) {
-      timeoutId = window.setTimeout(handleMouseUp, 1000);
+      timeoutId = window.setTimeout(handleCursorUp, 1000);
     }
 
     return () => {
       window.clearTimeout(timeoutId);
     };
-  }, [mouseStart, handleMouseUp]);
+  }, [mouseStart, handleCursorUp]);
 
   return (
     <div className="pb-100% relative select-none">
@@ -48,10 +79,14 @@ export function EmojiCard({ emoji, onClick }: EmojiCardProps) {
           'flex flex-col items-center shadow rounded p-2 bg-white absolute top-0 w-full h-full focus:outline-none hover:bg-orange-300 transition duration-300 draw' +
           (mouseStart ? ' active' : '')
         }
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onTouchStart={handleMouseDown}
-        onTouchEnd={handleMouseUp}>
+        onMouseDown={handleCursorDown}
+        onMouseUp={handleCursorUp}
+        onTouchStart={handleTouchStart}
+        onTouchMove={handleTouchMove}
+        onTouchEnd={(e) => {
+          e.preventDefault();
+          handleCursorUp();
+        }}>
         <DisplayEmoji emoji={emoji} className="pb-8 h-full" />
         <span className="font-semibold text-gray-600 text-sm absolute bottom-2 truncate max-w-full px-2">
           {emoji.id}
