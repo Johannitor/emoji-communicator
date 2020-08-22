dcf3ab87c03d00fabad8ddf3a281521ae7d574f1
Johann Lochbaum <johannlochbaum@outlook.de>
2020-08-22 20:15:21 +0200

    Remove focus style on emoji table

--- src/components/emoji-table/EmojiCard.tsx
+++ src/components/emoji-table/EmojiCard.tsx
@@ -11,7 +11,7 @@ export function EmojiCard({ emoji, onClick }: EmojiCardProps) {
   return (
     <div className="pb-100% relative">
       <button
-        className="flex flex-col items-center shadow rounded p-2 bg-white absolute top-0 w-full h-full focus:outline-none hover:bg-orange-300 focus:bg-orange-300 transition duration-300"
+        className="flex flex-col items-center shadow rounded p-2 bg-white absolute top-0 w-full h-full focus:outline-none hover:bg-orange-300 transition duration-300"
         onClick={() => {
           onClick(emoji);
         }}>
